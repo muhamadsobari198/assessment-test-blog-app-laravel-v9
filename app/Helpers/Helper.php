727b3f44764ccf0b79ce23accf1c6685431a6611
ms198 <muhamadsobari198@gmail.com>
2022-10-13 18:04:26 +0700

    update helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
 
 function _generateToken($length = 50)
 {
@@ -53,6 +54,22 @@ function _checkSidebar($URL)
 	}
 
 	return true;
+}
+
+function __uploadFile($file, $path, $base_64 = false)
+{
+	$name = time();
+	$ext = $file->getClientOriginalExtension();
+	$newName = $name. '.'. $ext;
+	
+	Storage::putFileAs($path, $file, $newName);
+	$path = 'storage/'.$newName;
+	return $path;
+}
+
+function __deleteFile($filename)
+{
+    return Storage::delete('public/' . $filename);
 }
 
 
