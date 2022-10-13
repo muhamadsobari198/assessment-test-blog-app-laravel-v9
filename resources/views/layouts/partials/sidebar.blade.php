0c63b62c925e725517d023c73c6c0989e9a1ece3
ms198 <muhamadsobari198@gmail.com>
2022-10-13 20:36:45 +0700

    add global function

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -8,6 +8,15 @@
 		<ul class="nav">
 
 			<li class="nav-item nav-category">Master Data</li>
+			@if(_checkSidebar('admin/users'))
+				<li id="articles_nav_menu" class="nav-item @if(count(Request::segments()) > 0 && Request::segments()[1] == 'users') active @endif">
+					<a href="{{route('users')}}" class="nav-link">
+						<i class="fa-solid fa-user"></i>
+						<span class="link-title">Users</span>
+					</a>
+				</li>
+			@endif
+
 			@if(_checkSidebar('admin/articles'))
 				<li id="articles_nav_menu" class="nav-item @if(count(Request::segments()) > 0 && Request::segments()[1] == 'articles') active @endif">
 					<a href="{{route('articles')}}" class="nav-link">
