92b2ef8e239a3dd3b5c23648ee8fbcd686943e57
ms198 <muhamadsobari198@gmail.com>
2022-10-13 17:58:39 +0700

    add function store

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
+
+use Exception;
+
+use App\Models\User;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
@@ -22,4 +29,46 @@ class ArticleController extends Controller
     {
         return view('admin.master.article');
     }
+
+    public function store(ArticleRequest $request)
+    {
+
+        $user = User::where('token', $request->bearerToken())->first();
+
+        $otherRules = ['file'  => 'required|file|mimes:jpeg,png,jpg,gif|max:2048'];
+        $request->validate($otherRules);
+
+        try {
+
+            $request['created_by'] = $user->id;
+
+            if($request->file('file')) {
+                $request['image'] = __uploadFile(
+                    $request->file('file'),
+                    'public'
+                );
+            }
+
+            $article = Article::create($request->except('file'));
+            
+            Cache::flush();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Article berhasil ditambahkan.',
+                'data'    => $article
+            ], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Terdapat kesalahan pada sistem internal.',
+                'error'   => $e->getMessage()
+            ], 500);
+
+        }
+
+    }
+
 }
